905743cfd362f0735a060fc0545af477d33199cd
Puran Singh Rana <puransinghrana080@gmail.com>
2025-05-22 12:14:23 +0530

    products- get,put,delete byId

--- Controllers/product.js
+++ Controllers/product.js
@@ -3,21 +3,66 @@ import { Products } from "../Models/Product.js";
 
 //add product
 
-export const addProduct = async(req,res)=>{
-    const {title,description,price,category,qty,imgSrc}=req.body
-    try{
-        let product =await Products.create({title,description,price,category,qty,imgSrc})
-        res.json({message:"Product added Successfully...",product,success:true})
-
-    }
-    catch(error){
-        res.json({message:error.message})
-    }
-}
+export const addProduct = async (req, res) => {
+  const { title, description, price, category, qty, imgSrc } = req.body;
+  try {
+    let product = await Products.create({
+      title,
+      description,
+      price,
+      category,
+      qty,
+      imgSrc,
+    });
+    res.json({
+      message: "Product added Successfully...",
+      product,
+      success: true,
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
 
 //get products
-export const getProducts = async(req,res)=>{
-    let products = await Products.find().sort({createdAt:-1})
-    res.json({message:"All Products",products,success:true})
+export const getProducts = async (req, res) => {
+  let products = await Products.find().sort({ createdAt: -1 });
+  res.json({ message: "All Products", products, success: true });
+};
 
-}
+//find product by id
+export const getProductById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    let product = await Products.findById(id);
+    if (!product) return res.json({ message: "Invalid Id", success: false });
+    res.json({ message: "Specified Product ", product, success: true });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+//update product by id
+export const updateProductById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    let product = await Products.findByIdAndUpdate(id, req.body, { new: true });
+    if (!product) return res.json({ message: "Invalid Id", success: false });
+    res.json({ message: "Product has been updated ", product, success: true });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+//delete product by id
+export const deleteProductById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    let product = await Products.findByIdAndDelete(id);
+    if (!product) return res.json({ message: "Invalid Id", success: false });
+
+    res.json({ message: "Your Product has been deleted", succes: true });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
--- Routes/product.js
+++ Routes/product.js
@@ -1,13 +1,27 @@
-import express from "express"
-import { addProduct, getProducts } from "../Controllers/product.js"
+import express from "express";
+import {
+  addProduct,
+  deleteProductById,
+  getProductById,
+  getProducts,
+  updateProductById,
+} from "../Controllers/product.js";
 
-const router = express.Router()
+const router = express.Router();
 
 //add product
-router.post('/add',addProduct)
+router.post("/add", addProduct);
 
 //get products
-router.get('/all',getProducts)
+router.get("/all", getProducts);
 
+//get product by id
+router.get("/:id", getProductById);
 
-export default router
+//update product by id
+router.put("/:id", updateProductById);
+
+//delete product by id
+router.delete("/:id", deleteProductById);
+
+export default router;
