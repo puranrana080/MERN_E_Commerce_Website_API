b80af27fa90dc30bcdab12be89df48f7601fff0a
Puran Singh Rana <puransinghrana080@gmail.com>
2025-05-22 11:01:16 +0530

    user register , hash password

--- Controllers/user.js
+++ Controllers/user.js
@@ -3,7 +3,11 @@ import { User } from "../Models/User.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    let user = await User.create({ name, email, password });
+    let user = await user.findOne({ email });
+    if (user)
+      return res.json({ message: "User already available", success: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({ name, email, password: hashedPassword });
     res.json({
       message: "User registered successfully....",
       user,
